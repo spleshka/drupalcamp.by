0e67da47d0995d0a71f2ec8658fbb2a262e25f69
spleshka <spleshka@gmail.com>
2019-03-24 19:54:56 +0300

    Replaced newsletter.

--- reactjs/components/02_moleculas/Newsletter/index.js
+++ reactjs/components/02_moleculas/Newsletter/index.js
@@ -1,61 +1,53 @@
 import React from 'react';
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
+const socials = [
+  {
+    url: 'https://join.slack.com/t/drupal-belarus/shared_invite/enQtNTc1MTkwMTUxNzAyLWZlMjVlZmY0YmJlOGM3YzkxN2M0ZjNiZGUwNjhhMDBmY2Q0MDZjZTg4ZGUyNDFiZGM2MmVlY2YwZmJmYzE3Y2U',
+    imageURL: '/static/assets/img/social/slack.png',
+    label: 'Slack',
+  },
+  {
+    url: 'https://t.me/drupal_camp_belarus',
+    imageURL: '/static/assets/img/social/telegram.png',
+    label: 'Telegram',
+  },
+  {
+    url: 'https://twitter.com/drupalcampby',
+    imageURL: '/static/assets/img/social/twitter.png',
+    label: 'Twitter',
+  },
+  {
+    url: 'https://www.facebook.com/Drupal-Camp-Belarus-561950297656804/',
+    imageURL: '/static/assets/img/social/facebook.png',
+    label: 'Facebook',
+  },
+];
+
+
 class Newsletter extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      EMAIL: '',
-    };
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(e) {
-    this.setState({ EMAIL: e.target.value });
-  }
 
   render() {
     return (
       <section id="sign_up" className="section  overlay light-text align-center">
         <div className="container">
           <h2>Keep me <span className="highlight">updated</span></h2>
-          <small>No Spam - Only latest news, price and activity updates</small>
-          <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <MailchimpSubscribe
-              url="https://drupalcamp.us19.list-manage.com/subscribe/post?u=8c355a91003793c0bc94a4ff6&id=b8305b7949"
-              render={({ subscribe, status, message }) => (
-                  <form id="subscribe_main" encType="multipart/form-data" className="form row newsletter-form mailchimp-form">
-                    {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
-                    {status === "success" && <div style={{ color: "#1ac6ff" }} >Subscribed! We will keep you posted.</div>}
-                    <fieldset className="col-sm-8">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        placeholder="your@email.com"
-                        onChange={this.onChange}
-                      />
-                    </fieldset>
-                    <fieldset className="col-sm-4">
-                      <input
-                        type="submit"
-                        disabled={status === 'success' || status === 'sending' ? 'disabled' : ''}
-                        value={status === 'success' ? 'success!' : status === 'sending' ? 'sending...' : 'submit'}
-                        className="btn btn-sm btn-outline-clr"
-                        onClick={e => {
-                          subscribe(this.state);
-                          e.preventDefault();
-                        }}
-                      />
-                    </fieldset>
-                    <div className="response" />
-                  </form>
-              )}
-            />
+          <small>Subscribe to our latest news!</small>
+          <br/>
+          <br/>
+          <br/>
+
+          <div className="row">
+            {socials.map(social => (
+              <div className="col-md-3" style={{ marginBottom: '30px' }}>
+                <a href={social.url} target="_blank" style={{ color: 'white', textDecoration: 'none', display: 'block' }}>
+                  <img src={social.imageURL} title={social.label} alt={social.label} style={{ height: '96px'}} />
+                  <div>{social.label}</div>
+                </a>
+              </div>
+            ))}
           </div>
+
         </div>
       </section>
     );
